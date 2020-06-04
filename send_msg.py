88b7952174c9c58cfbf8031f9026ce1b17f5705a
Murodali Narzullaev <61009662+mrmdali@users.noreply.github.com>
2020-06-04 13:08:41 +0000

    Update send_msg.py

--- send_msg.py
+++ send_msg.py
@@ -3,6 +3,6 @@ from credentials import *
 
 client = Client(account_sid, auth_token)
 
-my_msg = "Murodali qalesan dusim"
+my_msg = "Assalome alekum, mening ismim Murodali"
 
 message = client.messages.create(to=my_cell, from_=my_twilio, body=my_msg)
